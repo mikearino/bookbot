22f1022ec9d3d0af64bd3b941e18b1d6ba187ded
Mike <mikeylofgren@gmail.com>
2025-11-25 22:30:33 -0800

    add number of words function

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+
+
 def get_book_text(path):
     with open(path) as p:
         return p.read()
 
+def number_or_words(input):
+    count = 0
+    words = input.split()
+    for word in words:
+        count += 1
+    print(f"Found {count} total words")
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    text = get_book_text("books/frankenstein.txt")
+    number_or_words(text)
 
 main()
